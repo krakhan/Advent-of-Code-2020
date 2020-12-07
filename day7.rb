f53678250e3e2f1a96e901e86fe7eeea6020da2b
aehoffer <aehoffer@gmail.com>
2020-12-07 05:27:38 -0500

    One more little thing.

--- day7.rb
+++ day7.rb
@@ -28,7 +28,7 @@ def containing_bag_count(rules, target_containing_counts, target_bag)
 	count
   end
   
-  rules.keys.select { |k| k != target_bag }.map { |k| bag_count_rec.call(k) }.select { |c| c > 0 }.length
+  rules.keys.reject { |k| k == target_bag }.map { |k| bag_count_rec.call(k) }.select { |c| c > 0 }.length
 end
 
 shiny_gold_containing_counts = {}
